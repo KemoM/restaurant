b5576a98c66645aec55553e677c86d385b2692e4
KemoM <kemal.muhovic@gmail.com>
2017-09-10 21:15:47 +0200

    test 2

--- Restaurant.Web/Controllers/SampleDataController.cs
+++ Restaurant.Web/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace Restaurant_Web.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Commit test"
         };
 
         [HttpGet("[action]")]
